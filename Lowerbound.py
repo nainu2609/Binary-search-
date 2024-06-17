a6d2103651191f69e3ead8409dc81dd341f6841e
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-06-17 22:37:19 +0530

    lower bound

---
+++ Lowerbound.py
@@ -0,0 +1,15 @@
+#defination of lower bound is that smallest index such that arr[ind]>=x
+def lower_bound(arr, x):
+    left, right = 0, len(arr) - 1
+    ans = len(arr)
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >=x: 
+            ans= mid 
+            right = mid - 1
+        else:                     
+            left=mid+1 
+    return ans 
+arr = [1,2,3,4,5]
+x= 2
+print(lower_bound(arr, x))
